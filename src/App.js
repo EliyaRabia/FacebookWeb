f93d2277168e36c2129e6069a8124ee9787bd49c
EliyaRabia <eliyarabia@gmail.com>
2024-02-03 13:29:32 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import RouterLinking from './RouterLinking'
-// import LoginPage from './Login/LoginPage';
 function App() {
   return (
     <div className="App">
     <RouterLinking>
-
     </RouterLinking>
-      {/* <LoginPage /> */}
     </div>
   );
 }
