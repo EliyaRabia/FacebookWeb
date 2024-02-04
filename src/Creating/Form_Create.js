e35efe020ac492d2566f652523859b8860e94d6d
orshmuel9 <orshmuel9@gmail.com>
2024-02-04 19:52:12 +0200

    Create Form Add

--- src/Creating/Form_Create.js
+++ src/Creating/Form_Create.js
@@ -2,17 +2,83 @@ import { useNavigate } from "react-router-dom";
 import React, { useState , useRef } from "react";
 import "./Form_Create.css"
 function Form_Create() {
-    const navigate = useNavigate();
-    const usernameBox = useRef(null);
-    const passwordBox = useRef(null);
-    const passwordCheckBox = useRef(null);
-    const displayName = useRef(null);
-    const photo = useRef(null);
 
-    const handleSignUp = () => {
+    const [passwordMessage, setPasswordMessage] = useState("");
+    const navigate = useNavigate();
+    const usernameBox = useRef("");
+    const passwordBox = useRef("");
+    const passwordCheckBox = useRef("");
+    const displayName = useRef("");
+    const photo = useRef("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [userList,setuserList] = useState([
+        { username: "user1", password: "Password1!" ,displayName: "aaa" },
+        { username: "user2", password: "Password2!" ,displayName: "bbb" },
+    ]);
+    const handleTogglePassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+    const handlePasswordClick = () => {
+        setPasswordMessage(
+            passwordBox.current.value.length > 0
+            ? ""
+            : "At least 8 digits long, a combination of characters and letters, and capital letter"
+        );
+    };
+    const handlePasswordBlur = () => {
+        setPasswordMessage("");
+    };
+    /*
+    check the validation of the password
+    */
+    const validatePassword = (password) => {
+        // Check if the password has a length of 8 or more characters
+        const hasMinLength = password.length >= 8;
+        // Regular expressions to check for capital letter and special character
+        const hasCapitalLetter = /[A-Z]/.test(password);
+        // Include a broader range of special characters
+        const hasSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password);
+        // Password is valid if it has a capital letter and a special character
+        return hasMinLength && hasCapitalLetter && hasSpecialCharacter;
+    };
+    const goBack = () => {
         navigate("/");
-        console.log(usernameBox.current.value)
         };
+    const createUser = () =>{
+        const isUsernameExists = userList.some(
+            (user) => user.username === usernameBox.current.value
+        );
+        //console.log(usernameBox.current.value);
+        //console.log(passwordBox.current.value);
+        //console.log(passwordCheckBox.current.value);
+        //console.log(validatePassword(passwordBox.current.value))
+        if(usernameBox.current.value != "" && passwordBox.current.value !="" && passwordCheckBox.current.value !="" && displayName.current.value !="" ){
+            if(isUsernameExists == false){
+                if(validatePassword(passwordBox.current.value) == true){
+                    if(passwordBox.current.value == passwordCheckBox.current.value){
+                        const newUser = { username: usernameBox.current.value ,
+                            password: passwordBox.current.value ,
+                            displayName: displayName.current.value  };
+                        const updatedUserList = [...userList, newUser];
+                        setuserList(updatedUserList);
+                    }
+                    else{
+                        alert("password dont match!")
+                    }
+                }
+                else{
+                    alert("write valid password")
+                }
+            }
+            else{
+                alert("username already exists")
+            }
+        }
+        else{
+            alert("need to fill all the information")
+        }
+    };
+    console.log(userList);
     return(
         <div>
             <div>
@@ -23,7 +89,8 @@ function Form_Create() {
             </div>
             <div className="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Username</label>
-                <input type="text"
+                <input
+                type="text"
                 className="form-control"
                 placeholder="email or phone number"
                 aria-label="Username"
@@ -31,11 +98,55 @@ function Form_Create() {
                 ref={usernameBox}>
                 </input>
             </div>
-            <div>
-                <button type="button" className="button-container" onClick={handleSignUp}>Sign Up</button>
+            <div className="mb-3" >
+                <label for="exampleFormControlInput1" className="form-label">Password</label>
+                <input
+                type={showPassword ? 'text' : 'password'}
+                class="form-control"
+                placeholder="enter password"
+                aria-label="Password"
+                aria-describedby="addon-wrapping"
+                ref={passwordBox}
+                onClick={handlePasswordClick}
+                onBlur={handlePasswordBlur}>
+                </input>
+                <button type="button" className="toggle-button-container ml-2" onClick={handleTogglePassword}>
+                    {showPassword ? 'Hide' : 'Show'}
+                </button>
+            </div>
+            {passwordMessage && <div className="message">{passwordMessage}</div>}
+            <div className="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Confirm Password</label>
+                <input
+                type="password"
+                className="form-control"
+                placeholder="confirm password"
+                aria-label="Password"
+                aria-describedby="addon-wrapping"
+                onClick={handlePasswordClick}
+                ref={passwordCheckBox}
+                />
+            </div>
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Choose pic</label>
+                <input class="form-control" type="file" id="image" accept="image/*" ref={photo} />
+            </div>
+            <div className="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Display Name</label>
+                <input
+                type="text"
+                className="form-control"
+                placeholder="enter display name"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
+                ref={displayName}>
+                </input>
             </div>
             <div>
-                <button type="button" className="button-container" onClick={handleSignUp}>Go Back</button>
+                <button type="button" className="button-container" onClick={createUser}>Sign Up</button>
+            </div>
+            <div>
+                <button type="button" className="button-container" onClick={goBack}>Go Back</button>
             </div>
         </div>
     );
