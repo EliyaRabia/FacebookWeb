36eb0d0b62ee7f506b8624b58e88df2aaf2213d2
EliyaRabia <eliyarabia@gmail.com>
2024-02-08 15:51:31 +0200

    update

--- src/Pid/Pid.js
+++ src/Pid/Pid.js
@@ -1,5 +1,6 @@
 import Post from "./Post/Post";
 import Posts from "../data/db.json"
+import AddPost from "./AddPost/AddPost"
 
 import { useNavigate } from "react-router-dom";
 import React, { useState , useRef } from "react";
@@ -12,11 +13,18 @@ function Pid() {
   const [postList,setPostList] = useState(Posts); 
   return (
     <div>
-    {postList.map((post) =>
-      <Post {...post}></Post>
-    )}
-      
-      <button type="button" className="button-container" onClick={goBack}>Go Back</button>
+      <div>
+        <AddPost setPosts={setPostList} posts={postList}></AddPost>
+      </div>
+      <div>
+        {postList.map((post) => (
+          <Post {...post}></Post>
+        ))}
+
+        <button type="button" className="button-container" onClick={goBack}>
+          Go Back
+        </button>
+      </div>
     </div>
   );
 }
