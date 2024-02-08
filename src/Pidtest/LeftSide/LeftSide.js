a78e3908cd3599e23c3548a6e8faaf174b17efb8
ofekyem <ofekyemini87@gmail.com>
2024-02-08 19:04:51 +0200

    add components to left

--- src/Pidtest/LeftSide/LeftSide.js
+++ src/Pidtest/LeftSide/LeftSide.js
@@ -1,52 +1,12 @@
 import "./LeftSide.css"
+import UpOptions from "./UpOptions/UpOptions"; 
+import DownOptions from "./DownOptions/DownOptions";
 
 function LeftSide(){
     return(
         <div class="leftbar">
-            <div class="up_options">
-                <ul class="list-group">
-                    <li class="list-group-item d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z"></path></svg>
-                        <span class="m-2">Your Profile</span>
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3.5 8a5.5 5.5 0 1 1 8.596 4.547 9.005 9.005 0 0 1 5.9 8.18.751.751 0 0 1-1.5.045 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.499-.044 9.005 9.005 0 0 1 5.9-8.181A5.496 5.496 0 0 1 3.5 8ZM9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm8.29 4c-.148 0-.292.01-.434.03a.75.75 0 1 1-.212-1.484 4.53 4.53 0 0 1 3.38 8.097 6.69 6.69 0 0 1 3.956 6.107.75.75 0 0 1-1.5 0 5.193 5.193 0 0 0-3.696-4.972l-.534-.16v-1.676l.41-.209A3.03 3.03 0 0 0 17.29 8Z"></path></svg>
-                        <span class="m-2">Friends</span>
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M5 3.75C5 2.784 5.784 2 6.75 2h10.5c.966 0 1.75.784 1.75 1.75v17.5a.75.75 0 0 1-1.218.586L12 17.21l-5.781 4.625A.75.75 0 0 1 5 21.25Zm1.75-.25a.25.25 0 0 0-.25.25v15.94l5.031-4.026a.749.749 0 0 1 .938 0L17.5 19.69V3.75a.25.25 0 0 0-.25-.25Z"></path></svg>
-                        <span class="m-2">Saved</span>
-                    </li>
-                    <li class="list-group-item d-flex  align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M8.75 19.25a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0ZM15 4.75a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0Zm-12.5 0a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0ZM5.75 6.5a1.75 1.75 0 1 0-.001-3.501A1.75 1.75 0 0 0 5.75 6.5ZM12 21a1.75 1.75 0 1 0-.001-3.501A1.75 1.75 0 0 0 12 21Zm6.25-14.5a1.75 1.75 0 1 0-.001-3.501A1.75 1.75 0 0 0 18.25 6.5Z"></path><path d="M6.5 7.75v1A2.25 2.25 0 0 0 8.75 11h6.5a2.25 2.25 0 0 0 2.25-2.25v-1H19v1a3.75 3.75 0 0 1-3.75 3.75h-6.5A3.75 3.75 0 0 1 5 8.75v-1Z"></path><path d="M11.25 16.25v-5h1.5v5h-1.5Z"></path></svg>
-                        <span class="m-2">Groups</span>
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M0 4.75C0 3.784.784 3 1.75 3h20.5c.966 0 1.75.784 1.75 1.75v14.5A1.75 1.75 0 0 1 22.25 21H1.75A1.75 1.75 0 0 1 0 19.25Zm1.75-.25a.25.25 0 0 0-.25.25v14.5c0 .138.112.25.25.25h20.5a.25.25 0 0 0 .25-.25V4.75a.25.25 0 0 0-.25-.25Z"></path><path d="M9 15.584V8.416a.5.5 0 0 1 .77-.42l5.576 3.583a.5.5 0 0 1 0 .842L9.77 16.005a.5.5 0 0 1-.77-.42Z"></path></svg>
-                        <span class="m-2">Video</span>
-                    </li>
-                </ul>
-            </div>
-            <div class="down_options">
-                <div class="shortcut_title">
-                    <p>Your Shortcuts</p> 
-                    <a href ="">edit</a>
-                </div>
-                <ul class="list-group">
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="m-2">Shortcut 1</span>
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="m-2">Shortcut 2</span>
-                    </li> 
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="m-2">Shortcut 3</span>
-                    </li> 
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="m-2">Shortcut 4</span>
-                    </li>
-                </ul>
-            </div>
+            <UpOptions></UpOptions>
+            <DownOptions></DownOptions>
         </div>
     );
 } 
