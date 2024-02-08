da45cf28b6af7cb28f759686be599744c7b501e2
EliyaRabia <eliyarabia@gmail.com>
2024-02-08 10:47:41 +0200

    update

--- src/Pid/Pid.js
+++ src/Pid/Pid.js
@@ -1,7 +1,16 @@
+import Post from "./Post/Post";
+import Posts from "../data/db.json"
+
 function Pid() {
   return (
     <div>
       <h1>hiiiii</h1>
+      <div>
+        {
+          Posts.map((post) => 
+            <Post {...post}></Post>
+        )}
+      </div>
     </div>
   );
 }
