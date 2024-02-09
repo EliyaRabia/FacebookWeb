911cbd21ff919629f8c7920a965e53ee93c3f141
EliyaRabia <eliyarabia@gmail.com>
2024-02-09 17:55:45 +0200

    update pid

--- src/Pid/Pid.js
+++ src/Pid/Pid.js
@@ -6,10 +6,37 @@ import NaviBar from './NaviBar/NaviBar';
 import LeftSide from './LeftSide/LeftSide';
 import RightSide from "./RightSide/RightSide";
 
-import { useNavigate } from "react-router-dom";
 import React, { useState , useRef } from "react";
 function Pid({ setUserLoggedIn, userLoggedIn}) { 
   const [postList,setPostList] = useState(Posts); 
+
+  const handleDeletePost = (postId) => {
+    const updatedPosts = postList.filter((post) => post.id !== postId);
+    setPostList(updatedPosts);
+  };
+
+  const handleDeletePicture = (postId) => {
+    const updatedPostList = postList.map((post) => {
+      if (post.id === postId) {
+        return { ...post, pictures: "" };
+      } else {
+        return post;
+      }
+    });
+    setPostList(updatedPostList);
+  };
+
+   const handleAddPicture = (postId,photo) => {
+     const updatedPostList = postList.map((post) => {
+       if (post.id === postId) {
+         return { ...post, pictures: photo };
+       } else {
+         return post;
+       }
+     });
+     setPostList(updatedPostList);
+   };
+
   return (
     <div className="container-fluid">
       <div className="navbar-fixed">
@@ -25,14 +52,21 @@ function Pid({ setUserLoggedIn, userLoggedIn}) {
         <div className="col pidCol">
           <div>
             <div>
-              <AddPost setPosts={setPostList} posts={postList} userLoggedIn={userLoggedIn}></AddPost>
+              <AddPost
+                setPosts={setPostList}
+                posts={postList}
+                userLoggedIn={userLoggedIn}
+              ></AddPost>
             </div>
             <div>
               {postList.map((post) => (
                 <Post
                   key={post.id}
                   {...post}
+                  deletePost={handleDeletePost}
+                  deletePicture={handleDeletePicture}
                   userLoggedIn={userLoggedIn}
+                  addPicture={handleAddPicture}
                 ></Post>
               ))}
             </div>
--- src/Pid/Post/Post.js
+++ src/Pid/Post/Post.js
@@ -1,35 +1,159 @@
-import React from "react";
+import React, { useState,useRef } from "react";
 import "./Post.css";
 import PostManagement from "./PostManagement/PostManagement";
 
 function Post({
-  key,
   id,
   fullname,
   icon,
-  text,
+  initialText,
   pictures,
   time,
   likes,
   commentsNumber,
   comments,
-  setPost,
+  deletePost,
+  deletePicture,
+  addPicture,
   userLoggedIn,
 }) {
-  const iconUrl = icon instanceof File ? URL.createObjectURL(icon) : icon;
-  const picturesUrl = pictures instanceof File ? URL.createObjectURL(pictures) : pictures;
+    const [text, setText] = useState(initialText);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedText, setEditedText] = useState(text);
+    const fileInput = useRef(null);
+    const [photo, setPhoto] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const iconUrl = icon instanceof File ? URL.createObjectURL(icon) : icon;
+    const picturesUrl =
+      pictures instanceof File ? URL.createObjectURL(pictures) : pictures;
+
+
+
+
+  const handleEditText = () => {
+    setIsEditing(true);
+    setEditedText(text);
+  };
+
+  const handleSaveText = () => {
+    setIsEditing(false);
+    setText(editedText);
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+    setEditedText(text);
+  };
+
+  const handleDeletePost = () => {
+    deletePost(id); 
+  };
+
+  const handleDeletePicture = () => {
+    deletePicture(id)
+  };
+
+  const handleAddPicture = () => {
+    addPicture(id, photo);
+  };
+
+  function handleButtonClick() {
+    fileInput.current.click();
+  }
+
+function handleFileChange(event) {
+  if (event.target.files.length > 0) {
+    const selectedFile = event.target.files[0];
+    const imageUrl = URL.createObjectURL(selectedFile);
+    setSelectedFile(selectedFile);
+    setPhoto(imageUrl);
+    addPicture(id, selectedFile);
+  }
+}
+
   return (
     <div className="post">
       <div className="card" style={{ width: "18rem" }}>
-      <div class="avatar">
-        {icon && <img src={iconUrl} class="avatar__img" alt="User"/>}
-        <h5 class="card-title">
-          {fullname} <br></br>
-          {time}
-        </h5> 
-      </div> 
-        <div class="card-body">
-          <p class="card-text">{text}</p>
+        <div className="topPost">
+          <div class="avatar">
+            {icon && <img src={iconUrl} class="avatar__img" alt="User" />}
+            <h5 class="card-title">
+              {fullname} <br></br>
+              {time}
+            </h5>
+          </div>
+          <div
+            class="btn-group"
+            role="group"
+            aria-label="Button group with nested dropdown"
+          >
+            <div class="btn-group" role="group">
+              <button
+                type="button"
+                class="btn edit"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i class="bi bi-three-dots-vertical"></i>
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  {pictures ? (
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        onClick={handleDeletePicture}
+                      >
+                        <i className="bi bi-trash3-fill"></i> Delete picture
+                      </a>
+                    </li>
+                  ) : (
+                    <li>
+                      <a className="dropdown-item" onClick={handleButtonClick}>
+                        <i className="bi bi-plus-lg"></i> Add picture
+                        <input
+                          type="file"
+                          id="fileInput"
+                          accept="image/*"
+                          style={{ display: "none" }}
+                          ref={fileInput}
+                          onChange={handleFileChange}
+                        ></input>
+                      </a>
+                    </li>
+                  )}
+                </li>
+                <li>
+                  <a class="dropdown-item" onClick={handleEditText}>
+                    <i class="bi bi-textarea-t"></i> Edit text
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" onClick={handleDeletePost}>
+                    <i class="bi bi-x-lg"></i> Delete post
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="card-body">
+          {isEditing ? (
+            <div>
+              <textarea
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+              ></textarea>
+              <button className="editButton" onClick={handleSaveText}>
+                <i class="bi bi-check-lg"></i>
+              </button>
+              <button className="editButton" onClick={handleCancelEdit}>
+                <i class="bi bi-trash3-fill"></i>
+              </button>
+            </div>
+          ) : (
+            <p className="card-text">{text}</p>
+          )}
         </div>
         {picturesUrl && (
           <img
