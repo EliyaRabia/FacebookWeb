85e488a601d69ecda66b92d5eb87a0349f12f97c
ofekyem <ofekyemini87@gmail.com>
2024-02-09 13:27:48 +0200

    add feautres to feed page

--- src/Pid/LeftSide/DownOptions/DownOptions.js
+++ src/Pid/LeftSide/DownOptions/DownOptions.js
@@ -2,24 +2,30 @@ import "./DownOptions.css"
 function DownOptions(){
     return(
         <div class="down_options">
-            <div class="shortcut_title">
-                <p>Your Shortcuts</p>
-                <a href="">edit</a>
+            <div class="leftbar_title">
+                <h4>Conversations</h4> 
+                <a href="">Hide Chat</a>
+            </div> 
+            <div class="online-list">
+                <div class="online">
+                    <img src="https://i.pinimg.com/736x/fa/60/51/fa6051d72b821cb48a8cc71d3481dfef.jpg"></img>
+                </div>
+                <p>Foo Bar</p>
             </div>
-            <ul class="list-group">
-                <li class="list-group-item d-flex align-items-center">
-                    <span class="m-2">Shortcut 1</span>
-                </li>
-                <li class="list-group-item d-flex align-items-center">
-                    <span class="m-2">Shortcut 2</span>
-                </li>
-                <li class="list-group-item d-flex align-items-center">
-                    <span class="m-2">Shortcut 3</span>
-                </li>
-                <li class="list-group-item d-flex align-items-center">
-                    <span class="m-2">Shortcut 4</span>
-                </li>
-            </ul>
+            <div class="online-list">
+                <div class="online">
+                    <img src="https://i.pinimg.com/736x/fa/60/51/fa6051d72b821cb48a8cc71d3481dfef.jpg"></img>
+                </div>
+                <p>Bob Alice</p>
+            </div> 
+            <div class="online-list">
+                <div class="online">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"></img>
+                </div>
+                <p>Mark Zuckerberg</p>
+            </div> 
+
+            
         </div>
     );
 }
