db418cd693c64c29704a6934e48b9338700159f3
EliyaRabia <eliyarabia@gmail.com>
2024-02-10 18:27:49 +0200

    tests

--- src/Creating/CreatingTests/Ctest1.test.js
+++ src/Creating/CreatingTests/Ctest1.test.js
@@ -1,4 +1,14 @@
-import { validatePassword } from '../Form_Create.js';
+// import { validatePassword } from '../Form_Create.js';
+const validatePassword = (password) => {
+  // Check if the password has a length of 8 or more characters
+  const hasMinLength = password.length >= 8;
+  // Regular expressions to check for capital letter and special character
+  const hasCapitalLetter = /[A-Z]/.test(password);
+  // Include a broader range of special characters
+  const hasSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password);
+  // Password is valid if it has a capital letter and a special character
+  return hasMinLength && hasCapitalLetter && hasSpecialCharacter;
+};
 
 test('validatePassword returns true for a valid password', () => {
     expect(validatePassword("Password123!")).toBe(true);
